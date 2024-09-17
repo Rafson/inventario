aca76b80cf112ee47abd4512824560fa0378c520
Rafson Silva <rafson@gmail.com>
2024-09-16 21:00:10 -0300

    Separando inventarion e usuario

---
+++ API/inventario.py
@@ -0,0 +1,67 @@
+import usuario
+from flask import Flask, jsonify, request, json, Response
+
+app = Flask(__name__)
+
+
+### USUARIO
+#LIST
+@app.route('/api/usuario/list', methods=['GET'])
+def user_list():
+    retorno_listaUsuarios = usuario.allUsers()
+    return jsonify(message=retorno_listaUsuarios)
+#GET USUARIO
+@app.route('/api/usuario', methods=['GET'])
+def user_get():
+    id = request.args[('id')]
+    retorno_usuario = usuario.usuario(id)
+    return jsonify(message=retorno_usuario)
+#UPDATE
+    #in Body select x-www-form-urlencoded
+    #key:user
+    #value:you_json
+    # {
+    #     "email": "usuario1@example.com",
+    #     "id": 1,
+    #     "senha": "senha1",
+    #     "status": 1,
+    #     "usuario": "usuario1"
+    # }
+@app.route('/api/usuario/update', methods=['POST'])
+def usuario_update():
+    atualizacao = request.form.get('update')
+    retorno_atualizacao = usuario.update(atualizacao)
+    print(f"************{retorno_atualizacao}")
+    return jsonify(message=retorno_atualizacao)
+#ADD
+@app.route('/api/usuario/add', methods=['POST'])
+def usuario_add():
+    retorno_add = usuario.add()
+    return jsonify(message=f"{retorno_add}")
+#DELETE
+@app.route('/api/usuario/delete', methods=['POST'])
+def usuario_delete():
+    print('DELETE')
+    retorno_delete = usuario.delete()
+    return jsonify(message=f"{retorno_delete}")
+    
+#LOGIN
+@app.route('/api/usuario/login', methods=['POST'])
+def usuario_login():
+    print('LOGIN')
+    retorno_login = usuario.login()
+    return jsonify(retorno_login)
+
+#TESTE
+@app.route('/api/usuario/teste', methods=['GET'])
+def usuario_teste():
+    dados = {
+            "nome": "João",
+            "idade": 30,
+            "cidade": "São Paulo",
+            "habilidades": ["Python", "Flask", "SQL"]
+        }
+    return Response(json.dumps(dados), mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- API/usuario.py
+++ API/usuario.py
@@ -1,63 +1,75 @@
 from flask import Flask, jsonify, request
 import sqlite3, json
-app = Flask(__name__)
 
-#MOSTRAR TODOS OS USUARIOS
-@app.route('/api/usuario/list', methods=['GET'])
-def allUsers():
+
+def allUsers(): #Lista de todos os usuários
     listaUsuarios = query("SELECT * FROM tbUsuario")
-    return jsonify(message=listaUsuarios)
-#MOSTRAR DADOS DO USURIO POR ID
-@app.route('/api/usuario', methods=['GET'])
-def usuario():
-    id = request.args[('id')]
-    retornoUsuario = query(f"SELECT * FROM tbUsuario WHERE id='{id}'")
-    return jsonify(message=f"TESTE->{retornoUsuario[0][3]}")
+    return listaUsuarios
 
-@app.route('/api/usuario/login', methods=['POST'])
-def login():
-    usuario = request.args.get('usuario')
-    senha = request.args.get('senha')
-    retornoUsuario = query(f"SELECT * FROM tbUsuario WHERE usuario='{usuario}' and senha='{senha}'")
-    return jsonify(message=f"{retornoUsuario}")
+def usuario(id): #Retorna os dados de 1 usuario
+    retornoUsuario = query(f"SELECT id, usuario, email, createdOn, status FROM tbUsuario WHERE id='{id}'")
+    return retornoUsuario
 
-#ATUALIZAR USUARIO JA EXISTENTE
-@app.route('/api/usuario/update', methods=['POST'])
-def update():
-    retorno=""
-    atualizacao = request.form.get('update')
-    #print(atualizacao)
-    dados_json = json.loads(str(atualizacao))
-    #print(dados_json['id'])
+def login(): #Realiza login com senha
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+    query_login = f"SELECT * FROM tbUsuario WHERE usuario='{usuario}' and senha='{senha}'"
+    print(query_login)
+    retornoUsuario = query(f"{query_login}")
+    return retornoUsuario
 
-    id = dados_json['id']
-    usuario = dados_json['usuario']
-    senha = dados_json['senha']
-    email = dados_json['email']
-    status= dados_json['status']
-
-    if id!="" and usuario!="" and senha!="" and email!="" and status!="":
-        query = f"UPDATE tbUsuario set senha='{senha}', email='{email}', status='{status}' WHERE id={id}"
-        print(query)
-        resposta = queryAddUpdate(query)
-        if resposta=="":
-            resposta="Atualizado com sucesso"
+def update(atualizacao): #Atualizar usuario ja existente
+    try:
+        retorno="OK"
+        dados_json = json.loads(str(atualizacao))
+        id = dados_json['id']
+        usuario = dados_json['usuario']
+        senha = dados_json['senha']
+        email = dados_json['email']
+        status= dados_json['status']
+        if id!="" and usuario!="" and senha!="" and email!="" and status!="":
+            query = f"UPDATE tbUsuario set senha='{senha}', email='{email}', status='{status}' WHERE id={id}"
+            print(query)
+            resposta = queryAddUpdate(query)
+            if resposta=="":
+                resposta="Atualizado com sucesso"
+            else:
+                resposta=f"Problema na inserção: {resposta}"
         else:
-            resposta=f"Problema na inserção: {resposta}"
+            resposta = "Execução falhou"
+        print(resposta)
+        
+    except sqlite3.OperationalError as e:
+        # Captura erros relacionados à operação do banco de dados (como tabela não encontrada)
+        retorno=f"Erro operacional: {e}"
+    except sqlite3.DatabaseError as e:
+        # Captura erros gerais de banco de dados
+        retorno=f"Erro de banco de dados: {e}"
+    except Exception as e:
+        # Captura qualquer outra exceção genérica
+        retorno=f"Ocorreu um erro inesperado: {e}"
+    return retorno
+def delete():
+    id = request.form.get('id')
+    usuario = request.form.get('usuario')
+    retorno = "Apagado com sucesso"
+    if usuario!="" and id!="":
+        query = f"DELETE FROM tbUsuario Where id={id} and usuario='{usuario}'"
+        print(query)
+        retorno = queryAddUpdate(query)
+        if retorno=="":
+            retorno="Apagado com sucesso"
+        else:
+            retorno=f"Problema ao Apagar: {retorno}"
     else:
-        resposta = "Execução falhou"
-
-    return jsonify(message=f"{retorno}")
-
-
-@app.route('/api/usuario/add', methods=['POST'])
-#INSERIR UM USUARIO NOVO
-def add():
+        retorno = "Execução falhou"
+    return retorno
+def add():#Inserir um usuario novo
     usuario = request.form.get('usuario')
     senha = request.form.get('senha')
     email = request.form.get('email')
     print(f"{usuario},{senha},{email}")
-    resposta=""
+    resposta="Atualizado com sucesso"
     if usuario!="" and senha!="" and email!="":
         query = f"INSERT INTO tbUsuario (usuario, senha, email, status) VALUES \
                 ('{usuario}', '{senha}', '{email}',1)"
@@ -69,7 +81,8 @@ def add():
             resposta=f"Problema na inserção: {resposta}"
     else:
         resposta = "Execução falhou"
-    return jsonify(message=f"{resposta}")
+    print(f"***********{resposta}")
+    return resposta;
 def queryAddUpdate(query):
     mensagemFinal = ""
     try:
@@ -107,8 +120,7 @@ def query(query):
     cursor.execute(query)
 
     # Recupera todos os resultados da consulta
-    rows = cursor.fetchall()
-
+    rows = cursor.fetchall() #TODO dar um jeito de fazer esse retorno ser JSON
     # Exibe os dados
     print("Dados da tabela:")
     # for row in rows:
@@ -117,5 +129,3 @@ def query(query):
 
     # Fecha a conexão com o banco de dados
     conn.close()
-if __name__ == '__main__':
-    app.run(debug=True)
